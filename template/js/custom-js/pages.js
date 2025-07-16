e9397040dabe67ad15432fc4b197d04174d10219
Eduardo <edu.vlemes@gmail.com>
2025-07-15 23:53:23 -0300

    .

--- template/js/custom-js/pages.js
+++ template/js/custom-js/pages.js
@@ -1,1 +1,11 @@
 // Add your custom JavaScript for storefront pages here.
+document.addEventListener('click', (e) => {
+  const btn = e.target.closest('[aside-toggle]');
+  if (btn) {
+    const asideClass = btn.getAttribute('aside-toggle');
+    const aside = document.querySelector(`.${asideClass}`);
+    if (aside) {
+      aside.classList.toggle('visible');
+    }
+  }
+});
