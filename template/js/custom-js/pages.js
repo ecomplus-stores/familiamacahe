c064ed15dc97f44a6952dce53adc9feb83e59767
Eduardo <edu.vlemes@gmail.com>
2025-07-22 18:22:58 -0300

    .

--- template/js/custom-js/pages.js
+++ template/js/custom-js/pages.js
@@ -9,3 +9,29 @@ document.addEventListener('click', (e) => {
     }
   }
 });
+
+
+document.addEventListener('click', function(e) {
+  const scrollBtn = e.target.closest('[data-scroll]');
+  if (scrollBtn) {
+    e.preventDefault();
+    const targetId = scrollBtn.getAttribute('data-scroll');
+    const targetEl = document.getElementById(targetId);
+    if (targetEl) {
+      // Calcular posição com offset de -30px
+      const elementPosition = targetEl.getBoundingClientRect().top + window.pageYOffset;
+      const offsetPosition = elementPosition - 30;
+      
+      window.scrollTo({
+        top: offsetPosition,
+        behavior: 'smooth'
+      });
+    } else {
+      if (scrollBtn.tagName === 'A' && scrollBtn.href) {
+        window.location.href = scrollBtn.href;
+      } else {
+        scrollBtn.click();
+      }
+    }
+  }
+});
